6bf761ac5ed1791c9fc3d3c0fbaa6d305b8c6376
Pichit Promsutipong <angelix.junk@gmail.com>
2016-05-01 16:27:04 +0700

    Update AI function

--- core/src/com/veil/ai/GameAI.java
+++ core/src/com/veil/ai/GameAI.java
@@ -5,12 +5,39 @@ public class GameAI {
 	
 	public static GameAI instance = new GameAI();
 
+	private int shootCounter = 0;
+	private boolean walkLeft = false;
+	private int walkCounter = 0;
 	private boolean holdJump = false;
 	
 	public void aiUpdate(Controller controller, LevelSnapshot info){
-		jump(controller, info);
-		if(holdJump){
-			controller.jump = true;
+		preControlUpdate(controller, info);
+	}
+	
+	private void preControlUpdate(Controller controller, LevelSnapshot info){
+		if(walkCounter > 0){
+			walkCounter--;
+		}
+		if(shootCounter > 0){
+			shootCounter--;
+		}
+	}
+	
+	private void walk(Controller controller, boolean left, int steps){
+		walkCounter = steps;
+		if(walkCounter > 0){
+			if(walkLeft){
+				controller.left = true;
+			}else{
+				controller.right = true;
+			}
+		}
+	}
+	
+	private void shoot(Controller controller, int frequency){
+		if(shootCounter == 0){
+			controller.shoot = true;
+			shootCounter = frequency+1; //We need at least 1 frame to release button 
 		}
 	}
 	
@@ -20,9 +47,12 @@ public class GameAI {
 				holdJump = false;
 			}
 		}else{
-			if(info.onFloor){
+			if(info.playerOnFloor){
 				holdJump = true;
 			}
+		}
+		if(holdJump){
+			controller.jump = true;
 		}
 	}
 }
--- core/src/com/veil/ai/LevelSnapshot.java
+++ core/src/com/veil/ai/LevelSnapshot.java
@@ -4,7 +4,9 @@ import java.util.HashMap;
 import java.util.List;
 
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 import com.veil.adl.EventFlag;
+import com.veil.game.GameConstant;
 import com.veil.game.element.DynamicEntity;
 import com.veil.game.element.Player;
 
@@ -12,17 +14,20 @@ public class LevelSnapshot {
 	
 	public Rectangle playerRect;
 	public EventFlag playerState;
-	public boolean onFloor;
+	public boolean playerOnFloor;
 	
 	public Rectangle enemyRect;
+	public Vector2 enemyDist;
 	public HashMap<DynamicEntity,Rectangle> tempRect;
+	public HashMap<DynamicEntity,Vector2> tempDist;
 	
 	public LevelSnapshot(Player player, List<DynamicEntity> permanent, List<DynamicEntity> temp){
 		playerRect = new Rectangle(player.getWorldCollider());
 		playerState = player.flag.cloneImportantFlag();
-		onFloor = player.onFloor();
+		playerOnFloor = player.onFloor();
 		if(permanent.size() > 0){
 			enemyRect = new Rectangle(permanent.get(0).getWorldCollider());
+			enemyDist = new Vector2();
 		}
 		tempRect = new HashMap<DynamicEntity,Rectangle>();
 		for(DynamicEntity dyn : temp){
