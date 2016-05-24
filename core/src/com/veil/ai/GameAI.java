1bc5d406fcd90164b28864926c3493ecdfffeb66
Pichit Promsutipong <angelix.junk@gmail.com>
2016-05-24 15:06:43 +0700

    Remove unused code

--- core/src/com/veil/ai/GameAI.java
+++ core/src/com/veil/ai/GameAI.java
@@ -27,21 +27,12 @@ public class GameAI {
 	
 	private HashMap<DynamicEntity, FrameHistoryBuffer> entityTracker = new HashMap<DynamicEntity, FrameHistoryBuffer>();
 	
-	private int goalX, goalY;
-	private boolean pressJump = false;
 	private boolean startFallingDown = false;
 	
 	public void aiUpdate(Controller controller, LevelSnapshot info, float delta){
-		//Setup frame data for prediction
 		setupFrameDataAndFlag(info);
 		searchButtonCombination(info, delta);
 		pressButton(controller, info, delta);
-		/*
-		//Set goal based on predicted enemy path
-		simulateAndSetGoal(info, delta);
-		//Move according to goal
-		moveToGoal(controller, goalX, goalY, info.playerRect, !info.playerState.surfaceInFront[2]);
-		*/
 	}
 	
 	private void setupFrameDataAndFlag(LevelSnapshot info){
@@ -218,103 +209,11 @@ public class GameAI {
 		//Require jumping, check if jumping should be delayed by 1 frame or not (release jumping button before pressing again)
 		if(buttonCombination[currentCombination][2]){
 			controller.jump = !startFallingDown;
-			/*
-			//On floor
-			if(info.playerState.surfaceInFront[2]){
-				//Release the button and try to jump again next frame
-				if(pressJump){
-					controller.jump = false;
-					pressJump = false;
-				}else{
-					controller.jump = true;
-					pressJump = true;
-				}
-			}else{
-				controller.jump = true; 
-			}
-			*/
 		}
 		DummyPlayer dummy = new DummyPlayer(info.level, 1);
 		dummy.mimicPlayer(info.player);
 		Rectangle[] rect = dummy.simulatePosition(controller.left, controller.right, controller.up, controller.down, controller.jump,
 						reactionTime, delta);
 		simulatedPlayerPos = rect;
-	}
-	
-	private void simulateAndSetGoal(LevelSnapshot info, float delta){
-		Rectangle[][] predictedFrames = new Rectangle[entityTracker.size()][];
-		predictedEnemyPos = new Rectangle[entityTracker.size()][];
-		int index=0;
-		for(DynamicEntity dyn : entityTracker.keySet()){
-			predictedFrames[index] = entityTracker.get(dyn).predictNextFrame(20);
-			predictedEnemyPos[index] = predictedFrames[index];
-			if(dyn == info.enemy){
-				bufferedEnemyPos = new Rectangle[entityTracker.get(dyn).getBuffer().size()];
-				int i=0;
-				for(Rectangle rect : entityTracker.get(dyn).getBuffer()){
-					bufferedEnemyPos[i] = rect;
-					i++;
-				}
-			}
-			index++;
-		}
-		int lowestCombinationCost = Integer.MAX_VALUE;
-		int combination = -1;
-		for(int i=0; i<buttonCombination.length; i++){
-			Rectangle player = simulatePlayerPlatformer(info, 
-					buttonCombination[i][0], buttonCombination[i][1], buttonCombination[i][2], delta);
-			int cost = 0;
-			index = 0;
-			for(DynamicEntity dyn : entityTracker.keySet()){
-				for(Rectangle enemy : predictedFrames[index]){
-					if(enemy.overlaps(player)){
-						cost++;
-					}
-				}
-				index++;
-			}
-			if(cost < lowestCombinationCost){
-				lowestCombinationCost = cost;
-				combination = i;
-			}
-		}
-		Rectangle goalRect = simulatePlayerPlatformer(info, buttonCombination[combination][0], 
-				buttonCombination[combination][1], buttonCombination[combination][2], delta);
-		goalX = Math.round(goalRect.x);
-		goalY = Math.round(goalRect.y);
-		
-		simulatedPlayerPos = new Rectangle[]{ goalRect };
-	}
-	
-	private void moveToGoal(Controller controller,int goalX, int goalY, Rectangle playerRect, boolean playerInAir){
-		int distX = goalX - (int)playerRect.x;
-		int distY = goalY - (int)playerRect.y;
-		if(Math.abs(distX) > GameConstant.speed){
-			if(distX < 0){
-				controller.left = true;
-			}else{
-				controller.right = true;
-			}
-		}
-		if(distY > 0){
-			if(!playerInAir){
-				//Release the button and try to jump again
-				if(pressJump){
-					pressJump = false;
-				}else{
-					controller.jump = true;
-					pressJump = true;
-				}
-			}else{
-				//Do not release jump button if goal is above
-				controller.jump = true; 
-			}
-		}
-	}
-	
-	private Rectangle simulatePlayerPlatformer(LevelSnapshot info, boolean left, boolean right, boolean jump, float simDelta){
-		DummyPlayer dummy = new DummyPlayer(info.level, 1);
-		dummy.mimicPlayer(info.player);
-		return dummy.simulatePosition(left, right, false, false, jump, reactionTime, simDelta)[reactionTime-1];
 	}
 }
