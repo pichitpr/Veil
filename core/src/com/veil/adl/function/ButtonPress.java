503d9568f4ec5dd589d829f49f1b0f180e572ac6
Pichit Promsutipong <angelix.junk@gmail.com>
2016-07-01 17:36:53 +0700

    Change ButtonPress function to support Controller

--- core/src/com/veil/adl/function/ButtonPress.java
+++ core/src/com/veil/adl/function/ButtonPress.java
@@ -5,6 +5,7 @@ import adl_2daa.SequenceInterpreter;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.veil.ai.Controller;
 
 public class ButtonPress implements IFunction{
 
@@ -13,17 +14,23 @@ public class ButtonPress implements IFunction{
 		String key = param[0].toString();
 		boolean result = false;
 		if(key.equalsIgnoreCase("up")){
-			result = Gdx.input.isKeyPressed(Input.Keys.UP);
+			result = Controller.instance.up;
+			//result = Gdx.input.isKeyPressed(Input.Keys.UP);
 		}else if(key.equalsIgnoreCase("down")){
-			result = Gdx.input.isKeyPressed(Input.Keys.DOWN);
+			result = Controller.instance.down;
+			//result = Gdx.input.isKeyPressed(Input.Keys.DOWN);
 		}else if(key.equalsIgnoreCase("left")){
-			result = Gdx.input.isKeyPressed(Input.Keys.LEFT);
+			result = Controller.instance.left;
+			//result = Gdx.input.isKeyPressed(Input.Keys.LEFT);
 		}else if(key.equalsIgnoreCase("right")){
-			result = Gdx.input.isKeyPressed(Input.Keys.RIGHT);
+			result = Controller.instance.right;
+			//result = Gdx.input.isKeyPressed(Input.Keys.RIGHT);
 		}else if(key.equalsIgnoreCase("attack")){
-			result = Gdx.input.isKeyPressed(Input.Keys.X);
+			result = Controller.instance.shoot;
+			//result = Gdx.input.isKeyPressed(Input.Keys.X);
 		}else if(key.equalsIgnoreCase("jump")){
-			result = Gdx.input.isKeyPressed(Input.Keys.Z);
+			result = Controller.instance.jump;
+			//result = Gdx.input.isKeyPressed(Input.Keys.Z);
 		}
 		return result;
 	}
